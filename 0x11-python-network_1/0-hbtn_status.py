fc8f1c007e0a59dd6389db54881876e92a71fb92
mwangii <jlmwangi254@gmail.com>
2024-03-30 11:25:50 +0300

    check task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# fetches https://alx-intranet.hbtn.io/status
+"""fetches https://alx-intranet.hbtn.io/status"""
 
 import urllib.request
 
@@ -9,9 +9,9 @@ def hbtn_status():
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
         html = r.read()
         print("Body response:")
-        print("\t- type:", type(html))
-        print("\t- content:", html)
-        print("\t- utf8 content:", html.decode('utf-8'))
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
 
 
 if __name__ == "__main__":
